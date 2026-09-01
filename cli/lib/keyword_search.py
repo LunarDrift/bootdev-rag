1e20afc00bd71ec1f5ade35b53f597f7e8adde20
Travis <mynameismyname1992@gmail.com>
2026-08-31 20:02:50 -0500

    Add tokenization functions

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -6,21 +6,38 @@ def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
     for movie in movies:
-        preprocessed_query = process_text(query)
-        preprocessed_title = process_text(movie["title"])
-        if preprocessed_query in preprocessed_title:
+        query_tokens = tokenize_text(query)
+        title_tokens = tokenize_text(movie["title"])
+        if has_matching_token(query_tokens, title_tokens):
             results.append(movie)
             if len(results) >= limit:
                 break
+
     return results
+
+
+def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
+    for query_token in query_tokens:
+        for title_token in title_tokens:
+            if query_token in title_token:
+                return True
+    return False
 
 
 def process_text(text: str) -> str:
     text = text.lower()
-
     # Remove punctuation
     text = text.translate(str.maketrans("", "", string.punctuation))
 
-    # Tokenization
-
     return text
+
+
+def tokenize_text(text: str) -> list[str]:
+    text = process_text(text)
+    tokens = text.split()
+    valid_tokens = []
+    for token in tokens:
+        if token:
+            valid_tokens.append(token)
+
+    return valid_tokens
